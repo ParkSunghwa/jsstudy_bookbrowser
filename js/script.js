7ba478d605de396723efdafcd615fec1300d2411
ParkSunghwa <mtreeferryp@gmail.com>
2022-07-25 02:22:36 +0900

    220725

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $(".button_search").click(function(){
+        // $(".temp").remove();
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: {query:$(".info_book").val()},
+            headers : {Authorization:"KakaoAK 348b84456b142bba3b1ba938fee6cc06"}
+          })
+            .done(function( msg ) {
+              $(".temp").append(msg.documents[0].title);
+              $(".temp").append("<img src ='"+ msg.documents[0].thumbnail + "'/>");
+            });
+
+
+
+    });
+
+
+
+
+
+});
